f0ae3f10c478bfca99e95f2b5cf8e40947edbd4e
Christopher Cosler <christopher@cosler.net>
2018-05-16 08:49:51 +0200

    Add neural network
    Does not perform as well as xgboost yet but hyperparameter tuning still needs to be done. Looks promising.

---
+++ 5_neural_net.R
@@ -0,0 +1,26 @@
+
+tfidf <- data.frame(as.matrix(tfidf))
+train_test <- cbind(train_test, tfidf) %>% dplyr::select(-txt)
+
+cat("Split into original train and test...\n")
+test <- train_test[((length(y)+1):nrow(train_test)),]
+train_test <- train_test[-((length(y)+1):nrow(train_test)),]
+train <- train_test ; rm(train_test) ; gc() # copying does not change memory usage
+train$y <- y
+
+# Train model -------------------------------------------------------------
+
+# From the benchmark experiments, we know that xgboost worked best and we have an idea about the parameters
+# As mlr cannot handle sparce matrices, we implement it for the whole dataset directly via xgboost
+
+cat("Training model...\n")
+
+net <- neuralnet::neuralnet(as.formula(paste0("y ~ ", paste0(names(train)[-ncol(train)], collapse = " + "))), data = train)
+pred <- neuralnet::compute(x = net, covariate = train[,-ncol(train)])
+print(sqrt(sum( ( pred$net.result - y)^2  ) / length(y)))
+# Create submission -------------------------------------------------------
+
+cat("Creating submission file in './submissions/'...\n")
+data.frame(item_id = item_id, deal_probability = pred) %>% write.csv(paste0("submissions/neuralnet_submission", "_", format(Sys.time(), format = '%Y%m%d_%H%M%S'), ".csv"), row.names = FALSE)
+
+                                                                     
---
+++ run_neural_net.R
@@ -0,0 +1,21 @@
+# This script performs training for xgboost, based on benchmarking results and hyperparameter tuning
+# in run_benchmarking.R
+
+# It uses sparse matrices and can thus handle the whole dataset even on smaller machines
+# The data wrangling scripts are shared with run_benchmarking.R however
+
+# The output can be found in "./submissions/..."
+
+# Configure ---------------------------------------------------------------
+
+# How many rows of the data to read from the train and test datasets
+nTrainData <- 1e5
+nTestData <- 1e7 # this must be >= obs in test data to be a valid submission
+
+
+# Run ---------------------------------------------------------------------
+
+source("0_setup.R")
+source("1_data.R")
+source("2_text.R")
+source("5_neural_net.R")
