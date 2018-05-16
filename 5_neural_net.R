bcd2b1643a89f71cc74e7c7a2102e5e2a1fc467b
Christopher Cosler <christopher@cosler.net>
2018-05-16 09:08:30 +0200

    Prediction added to neuralnet

--- 5_neural_net.R
+++ 5_neural_net.R
@@ -17,10 +17,16 @@ cat("Training model...\n")
 
 net <- neuralnet::neuralnet(as.formula(paste0("y ~ ", paste0(names(train)[-ncol(train)], collapse = " + "))), data = train)
 pred <- neuralnet::compute(x = net, covariate = train[,-ncol(train)])
+
+# RMSE
 print(sqrt(sum( ( pred$net.result - y)^2  ) / length(y)))
+
+rm(train) ; gc()
+
 # Create submission -------------------------------------------------------
+
+pred <- neuralnet::compute(x = net, covariate = test)$net.result
 
 cat("Creating submission file in './submissions/'...\n")
 data.frame(item_id = item_id, deal_probability = pred) %>% write.csv(paste0("submissions/neuralnet_submission", "_", format(Sys.time(), format = '%Y%m%d_%H%M%S'), ".csv"), row.names = FALSE)
 
-                                                                     
